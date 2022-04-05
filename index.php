f265f15ff163ffb464013087c047f0c6d6928e51
Mike Lawrence <mjalawrence1@gmail.com>
2022-04-05 17:05:33 +0100

    data displayed

--- index.php
+++ index.php
@@ -7,56 +7,94 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare("SELECT `name`, `country`, `city`, `dob`, `status`, `vibe`, `photo`
 FROM `skateplazas`;");
 $query->execute();
-$result = $query->fetchAll();
+$skatePlazas = $query->fetchAll();
 
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
+//echo '<pre>';
+//var_dump($skatePlazas);
+//echo '</pre>';
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href=".css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Skate Plazas Worldwide</title>
+</head>
+<body>
+<header>
+        <h1>Skate Plazas Worldwide</h1>
+</header>
+<main>
+    <?php
+    foreach($skatePlazas as $key => $plaza) {
+        $key++;
+        echo "<h1>Skate Plaza No. $key</h1>";
+        echo "<h1>{$plaza['name']}</h1>";
+        echo "<img src={$plaza['photo']}>";
+        echo "<p>Location: {$plaza['city']}, {$plaza['country']}.</p>";
+        echo "<p>Date of construction: {$plaza['dob']}.</p>";
+        if ($plaza['status'] !== "1") {
+            echo "<p>The good times are over. The authorities have already brought down the axe on this mecca of a by-gone era in skateboarding. RIP in peace.</p>";
+        } elseif ($plaza['vibe'] === "1") {
+            echo "<p>Only skaters hang out here so don’t expect much of a vibe beyond the skate session.</p>";
+        } elseif ($plaza['vibe'] ===  "2") {
+            echo "<p>As a skater, you share the space with other people on the lower rungs of society so act accordingly and keep an eye on your stuff.</p>";
+        } elseif ($plaza['vibe'] ===  "3") {
+            echo "<p>Due to its pleasant atmosphere, people on their lunchbreak tend to come here so be aware of stressed out workers trying to get away from their boss.</p>";
+        } elseif ($plaza['vibe'] ===  "4") {
+            echo "<p>It may be a surprise, but this place has some value outside of skateboarding so look out for people snapping photos and disregarding the established skatetiquette.</p>";
+        } else {
+            echo "<p>Somehow, whether by choice or ignorance, the powers that be have let the people define this spot, meaning it has a different character at all times of the day and night. Roll up and enjoy the vibe.</p>";
+        }
 
+    }
+    ?>
+</main>
+</body>
+</html>
 
-<!---->
-<!---->
-<!--//Display database code:-->
-<!---->
-<!--$name = null;-->
-<!--$country = null;-->
-<!--$city = null;-->
-<!--$DOB = null;-->
-<!--$skateStatus = null;-->
-<!--$generalVibe = null;-->
-<!--$photo = null;-->
-<!---->
-<!--foreach ($results as $indexedArray) {-->
-<!--    $name .= $key['name'];-->
-<!--    $country .= $key['country'];-->
-<!--    $city .= $key['city'];-->
-<!--    $DOB .= $key['DOB'];-->
-<!--    $skateStatus .= $key['skateStatus'];-->
-<!--    $generalVibe .= $key['generalVibe'];-->
-<!--    $photo .= $key['photo'];-->
-<!--        for ($i = 1; $i < 1000; $i++) {-->
-<!--            echo '<h1>"Skate Plaza No.$1"</h1>';-->
-<!--            echo '<img src="$photo"';-->
-<!--            echo '<br />';-->
-<!--            echo '<p>"Location: $city, $country."</p>';-->
-<!--            echo '<br />';-->
-<!--            echo '<p>"Date of construction: $DOB."</p>';-->
-<!--            echo '<br />';-->
-<!--            echo '<p>"Only skaters hang out here so don’t expect much of a vibe beyond the skate session."</p>';-->
-<!--                if ($skateStatus === 0) {-->
-<!--                    echo '<p>"The good times are over. The authorities have already brought down the axe on this mecca of a by-gone era in skateboarding. RIP in peace."</p>';-->
-<!--                } elseif ($generalVibe = 1) {-->
-<!--                    echo '<p>"Only skaters hang out here so don’t expect much of a vibe beyond the skate session."</p>';-->
-<!--                } elseif ($generalVibe = 2) {-->
-<!--                    echo '<p>"As a skater, you share the space with other people on the lower rungs of society so act accordingly and keep an eye on your stuff."</p>';-->
-<!--                } elseif ($generalVibe = 3) {-->
-<!--                    echo '<p>"Due to its pleasant atmosphere, people on their lunchbreak tend to come here so be aware of stressed out workers trying to get away from their boss."</p>';-->
-<!--                } elseif  ($generalVibe = 4) {-->
-<!--                    echo '<p>"It may be a surprise, but this place has some value outside of skateboarding so look out for people snapping photos and disregarding the established skatetiquette."</p>';-->
-<!--                } else {-->
-<!--                    echo '<p>"Somehow, whether by choice or ignorance, the powers at be have let the people define this spot, meaning it has a different character at all times of the day and night. Roll up and enjoy the vibe."</p>';-->
-<!--                }-->
-<!--            }-->
-<!--        }-->
+<!--//wot i rote b4 editin but didnt werk-->
+<?php
+//$name = null;
+//$country = null;
+//$city = null;
+//$DOB = null;
+//$skateStatus = null;
+//$generalVibe = null;
+//$photo = null;
+//
+//foreach ($result as $indexedArray => $key) {
+//    $name .= $key['name'];
+//    $country .= $key['country'];
+//    $city .= $key['city'];
+//    $DOB .= $key['DOB'];
+//    $skateStatus .= $key['skateStatus'];
+//    $generalVibe .= $key['generalVibe'];
+//    $photo .= $key['photo'];
+//
+//    for ($i = 1; $i < 1000; $i++) {
+//        echo '<h1>"Skate Plaza No.$1"</h1>';
+//        echo '<img src="$photo"';
+//        echo '<br />';
+//        echo '<p>"Location: $city, $country."</p>';
+//        echo '<br />';
+//        echo '<p>"Date of construction: $DOB."</p>';
+//        echo '<br />';
+//        echo '<p>"Only skaters hang out here so don’t expect much of a vibe beyond the skate session."</p>';
+//        if ($skateStatus === 0) {
+//            echo '<p>"The good times are over. The authorities have already brought down the axe on this mecca of a by-gone era in skateboarding. RIP in peace."</p>';
+//        } elseif ($generalVibe = 1) {
+//            echo '<p>"Only skaters hang out here so don’t expect much of a vibe beyond the skate session."</p>';
+//        } elseif ($generalVibe = 2) {
+//            echo '<p>"As a skater, you share the space with other people on the lower rungs of society so act accordingly and keep an eye on your stuff."</p>';
+//        } elseif ($generalVibe = 3) {
+//            echo '<p>"Due to its pleasant atmosphere, people on their lunchbreak tend to come here so be aware of stressed out workers trying to get away from their boss."</p>';
+//        } elseif ($generalVibe = 4) {
+//            echo '<p>"It may be a surprise, but this place has some value outside of skateboarding so look out for people snapping photos and disregarding the established skatetiquette."</p>';
+//        } else {
+//            echo '<p>"Somehow, whether by choice or ignorance, the powers at be have let the people define this spot, meaning it has a different character at all times of the day and night. Roll up and enjoy the vibe."</p>';
+//        }
+//    }
+?>
