a09e465a7c82ad8546e4da6b2bb1813840b5e1e5
Mike Lawrence <mjalawrence1@gmail.com>
2022-04-06 13:52:41 +0100

    it lives

--- functions.php
+++ functions.php
@@ -23,3 +23,38 @@ FROM `skateplazas`;");
     $skatePlazas = $query->fetchAll();
     return $skatePlazas;
 }
+
+/**
+ * Input database array, foreach it and display each item in a nice little box
+ *
+ * @param array $skatePlazas
+ * @return string
+ */
+function mainPageDisplay (array $skatePlazas): string
+{
+    $result = '';
+    foreach ($skatePlazas as $plaza) {
+        $result .= "<div class=plazas>";
+        $result .= "<h1>{$plaza['name']}</h1>";
+        $result .= "<div class=box style='background-image: url({$plaza['photo']})'>";
+        $result .= "</div>";
+        $result .= "<p>Location: {$plaza['city']}, {$plaza['country']}</p>";
+        $result .= "<p>Date of construction: {$plaza['dob']}</p>";
+        if ($plaza['status'] !== "1") {
+            $result .= "<p>The good times are over. The authorities have already brought down the axe on this mecca of a by-gone era in skateboarding. RIP in peace.</p>";
+        } elseif ($plaza['vibe'] === "1") {
+            $result .= "<p>Only skaters hang out here so don’t expect much of a vibe beyond the skate session.</p>";
+        } elseif ($plaza['vibe'] === "2") {
+            $result .= "<p>If you skate here you'll be sharing the space with other people on the lower rungs of society so act accordingly and keep an eye on your stuff.</p>";
+        } elseif ($plaza['vibe'] === "3") {
+            $result .= "<p>Due to its pleasant atmosphere, people on their lunchbreak tend to come here so be aware of stressed out workers trying to get away from their boss.</p>";
+        } elseif ($plaza['vibe'] === "4") {
+            $result .= "<p>It may be a surprise, but this place has some value outside of skateboarding so look out for people snapping photos and disregarding the established skatetiquette.</p>";
+        } else {
+            $result .= "<p>Somehow, whether by choice or ignorance, the powers that be have let the people define this spot, meaning it has a different character at all times of the day and night. Roll up and enjoy the vibe.</p>";
+        }
+        $result .= "</div>";
+    }
+    return $result;
+}
+?>
