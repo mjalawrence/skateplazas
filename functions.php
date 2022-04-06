22c944978f59b91384c997c51e1d25632a19caa2
Mike Lawrence <mjalawrence1@gmail.com>
2022-04-06 13:46:02 +0100

    story1 done 2.0

--- functions.php
+++ functions.php
@@ -1,1 +1,25 @@
 <?php
+/**
+ * Connects to the database
+ *
+ * @return PDO
+ */
+function getDbConnection(): PDO {
+    $db = new PDO('mysql:host=db; dbname=skateplazas', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $db;
+}
+
+/**
+ * Extracts the data and creates an array
+ *
+ * @param PDO $db
+ * @return array|false
+ */
+function getAllPlazas(PDO $db) {
+    $query = $db->prepare("SELECT `name`, `country`, `city`, `dob`, `status`, `vibe`, `photo`
+FROM `skateplazas`;");
+    $query->execute();
+    $skatePlazas = $query->fetchAll();
+    return $skatePlazas;
+}
