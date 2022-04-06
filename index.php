9c563fcc1b0928897b7e4f744ac3862e65f94e62
Mike Lawrence <mjalawrence1@gmail.com>
2022-04-06 15:07:13 +0100

    submit form

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ $skatePlazas = getAllPlazas($db);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Skate Plazas Worldwide</title>
 </head>
-<div>
 <header id="header">
         <h1>Skate Plazas Worldwide</h1>
 </header>
@@ -28,7 +27,56 @@ $skatePlazas = getAllPlazas($db);
     echo $itLives;
 
     ?>
+        <div class="plazas">
+            <h1>Submit New Plaza</h1>
+            <form>
+                <label>Name of Plaza:
+                    <input type="text" />
+                </label>
+                <label>Country:
+                    <input type="text" />
+                </label>
+                <label>City:
+                    <input type="text" />
+                </label>
+                <label>Year of Construction:
+                    <input type="text" />
+                </label>
+                <label>Is it a bust?
+                    <label>
+                        <input name="radio" type="radio"/>Yes
+                    </label>
+                    <label>
+                        <input name="radio" type="radio"/>No
+                    </label>
+                </label>
+                <label>Who hangs out there?</label>
+                    <label>
+                        <input name="radio" type="radio"/>Only skaters
+                    </label>
+                    <label>
+                        <input name="radio" type="radio"/>Sketchy people
+                    </label>
+                    <label>
+                        <input name="radio" type="radio"/>Lunch breakers
+                    </label>
+                    <label>
+                        <input name="radio" type="radio"/>Tourists
+                    </label>
+                    <label>
+                        <input name="radio" type="radio"/>Everybody: it's a party
+                    </label>
+
+                <label>Paste Photo URL:
+                    <input type="text" />
+                </label>
+                <div class="submit">
+                    <input type="submit" value="Submit Plaza"/>
+                </div>
+            </form>
+        </div>
     </div>
+
 </main>
     <div class="button">
         <a class ="hover_me" href="#header">Back to top</a>
